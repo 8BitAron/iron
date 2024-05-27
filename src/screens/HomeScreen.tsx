03fa00c7615741cba9c2577599f2d59594863072
Aron Braggans <aron.braggans@gmail.com>
2024-05-27 14:29:18 -0500

    adding locations, updated map, click to location

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,15 @@
+import Geolocation from '@react-native-community/geolocation';
 import {onlineManager} from '@tanstack/react-query';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import {useEffect, useRef, useState} from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import MapView, {Marker} from 'react-native-maps';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useLocations, usePuLocations} from '../api/locations/locations';
 import {ILocation} from '../components/locations/ILocation';
@@ -9,11 +18,52 @@ export function HomeScreen() {
   const {isError, data, isFetching} = useLocations();
   const isOnline = onlineManager.isOnline();
   const mutation = usePuLocations();
+  const mapRef = useRef(null);
+  const {width} = Dimensions.get('window');
+  const [widthOfMap, setWidthOfMap] = useState(width);
+  const [zoom, setZoom] = useState(15);
+
+  const [region, setRegion] = useState({
+    latitude: 44.933797,
+    longitude: -93.33666,
+    latitudeDelta: 0.0922 * 100,
+    longitudeDelta: 0.0421 * 100,
+  });
 
   const onClick = (location: ILocation) => {
     location.isDone = !location.isDone;
     mutation.mutate(location);
   };
+
+  const handleLocationClick = (location: ILocation) => {
+    //@ts-ignore
+    mapRef.current.animateToRegion(
+      {
+        latitude: location.latitude,
+        longitude: location.longitude,
+        latitudeDelta: 0.015 * 30,
+        longitudeDelta: 0.0121 * 30,
+      },
+      1000,
+    );
+  };
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        setRegion({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.015 * 5,
+          longitudeDelta: 0.0121 * 5,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  }, []);
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'blue'}}>
@@ -24,24 +74,49 @@ export function HomeScreen() {
         </View>
         {isFetching && <Text>Loading...</Text>}
         {isError && <Text>Error</Text>}
-        <MapView style={styles.map} />
+        <MapView
+          mapPadding={{top: 5, right: 5, bottom: 5, left: 5}}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          zoomEnabled={true}
+          zoomControlEnabled={true}
+          style={styles.map}
+          initialRegion={region}
+          onRegionChangeComplete={region => setRegion(region)}
+          onMapReady={() => {
+            setWidthOfMap(widthOfMap - 1);
+          }}
+          ref={mapRef}>
+          {data &&
+            data.map((item: ILocation, index: number) => (
+              <Marker
+                key={item.id}
+                coordinate={{
+                  latitude: item.latitude,
+                  longitude: item.longitude,
+                }}
+                title={item.title}
+                description={item.description}
+                onPress={() => onClick(item)}
+              />
+            ))}
+        </MapView>
+
+        <View style={{flex: 1}}>
+          {/*Display user's current region:*/}
+          <Text>Current Pin latitude: {region.latitude}</Text>
+          <Text>Current Pin longitude: {region.longitude}</Text>
+        </View>
+
         {data &&
           data.map((item: ILocation, index: number) => (
-            <View key={item.id}>
-              <View>
+            <View key={item.id} style={{flex: 1}}>
+              <TouchableOpacity onPress={() => handleLocationClick(item)}>
                 <Text>{item.title}</Text>
                 <Text>{item.description}</Text>
-                <Text>{item.isDone ? 'Done' : 'Not Done'}</Text>
-                <Text>{!item.isNotSynced ? 'Not Synced' : 'Synced'}</Text>
-              </View>
-              <View>
-                <Button
-                  title="Done"
-                  onPress={() => {
-                    onClick(item);
-                  }}
-                />
-              </View>
+                <Text>lat: {item.latitude}</Text>
+                <Text>long: {item.longitude}</Text>
+              </TouchableOpacity>
             </View>
           ))}
       </View>
@@ -55,6 +130,6 @@ const styles = StyleSheet.create({
   },
   map: {
     width: '100%',
-    height: '100%',
+    height: '50%',
   },
 });
